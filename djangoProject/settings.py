d57ffdaa673c7684f5798552ada4ddc5d773e91f
marcelino <marcelinomaged25@gmail.com>
2026-08-02 13:31:56 +0300

    Fix Vercel deployment: add api handler, logging config, and simplified vercel.json

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -167,3 +167,18 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
+
