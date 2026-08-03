b9ce952a4f7ba1c7e7836e0bdd2746349b59633b
marcelino <marcelinomaged25@gmail.com>
2026-08-04 00:36:41 +0300

    Serve uploaded media in production

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import RedirectView
+from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -26,7 +27,8 @@ urlpatterns = [
     path('market/', include('market.urls', namespace='market')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    # WhiteNoise serves static assets, while uploaded media needs its own route in production.
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
-urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
